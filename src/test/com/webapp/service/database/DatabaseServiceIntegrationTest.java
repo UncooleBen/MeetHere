2d7c642d2fa582bc09c76eae5a89b25587ef407a
uncooleben <pengjuntao2016@foxmail.com>
2019-12-16 16:06:29 +0800

    Added Integration Test Directory

---
+++ src/test/com/webapp/service/database/DatabaseServiceIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.webapp.service.database;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseServiceIntegrationTest {
+    @Test
+    void test() {
+        
+    }
+
+}

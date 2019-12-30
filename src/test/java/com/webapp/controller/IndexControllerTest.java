582d1e270b1f2e2750c74c0ec874a6f88d9c38f2
UncooleBen <pengjuntao2016@foxmail.com>
2019-12-30 15:49:40 +0800

    Fixed a login unit test bug

--- src/test/java/com/webapp/controller/IndexControllerTest.java
+++ src/test/java/com/webapp/controller/IndexControllerTest.java
@@ -3,9 +3,11 @@ package com.webapp.controller;
 import org.junit.jupiter.api.Test;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class IndexControllerTest {
 
@@ -15,6 +17,8 @@ class IndexControllerTest {
     @Test
     public void service_test() {
         HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpSession session = mock(HttpSession.class);
+        when(request.getSession()).thenReturn(session);
         assertEquals(indexController.service(request), "index");
     }
 
